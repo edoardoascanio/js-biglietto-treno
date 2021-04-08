1f94973dfd11b3526b9db44a5f1a5e220cc8243c
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-08 16:35:48 +0200

    controllo numeri

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ console.log("Prezzo Totale" + prezzoTotale + "£");
 var prezzo = (Math.random() * 10); 
 prezzo.toFixed(2);
 
+if(Number.isNaN (numeroChilometri) || Number.isNaN (età)){
+     alert("Inserire cifre numeriche")
+     }
+
 if(età < 18){
    var scontoMinorenni = ((prezzoTotale * 20) / 100);
    console.log("Sconto Minorenni" + scontoMinorenni + "£")
