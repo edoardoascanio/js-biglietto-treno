587245d7c1434d04b9c7e4b9d961b9d02330b28d
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-08 16:17:28 +0200

    add sconti

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var numeroChilometri = parseInt (prompt("Scrivi il numero dei chilometri che vuoi percorrere"))
 console.log(numeroChilometri + "Km");
 
-var età = parseInt (prompt("Scrivi la tua età"))
+var età = parseInt (prompt("Scrivi la tua età"));
 console.log(età + "anni");
 
-var prezzoAlChilometro  = (0.21)
+var prezzoAlChilometro  = (0.21);
 
-var prezzoTotale = (numeroChilometri * prezzoAlChilometro)
-console.log("Prezzo Totale" + prezzoTotale + "£")
-var prezzo = (Math.random() * 10) 
-prezzo.toFixed(2)
+var prezzoTotale = (numeroChilometri * prezzoAlChilometro);
+console.log("Prezzo Totale" + prezzoTotale + "£");
+var prezzo = (Math.random() * 10); 
+prezzo.toFixed(2);
+
+if(età < 18){
+   var scontoMinorenni = ((prezzoTotale * 20) / 100);
+   console.log("Sconto Minorenni" + scontoMinorenni + "£")
+}
+
+if(età > 65){
+    var scontoOver = ((prezzoTotale * 40) / 100);
+    console.log("Sconto Over" + scontoOver + "£")
+ }
