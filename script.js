32f51590d8051f37b1dbc8c2914538397c46fadd
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-08 15:57:12 +0200

    add prezzo totale

---
+++ script.js
@@ -0,0 +1,12 @@
+var numeroChilometri = parseInt (prompt("Scrivi il numero dei chilometri che vuoi percorrere"))
+console.log(numeroChilometri + "Km");
+
+var età = parseInt (prompt("Scrivi la tua età"))
+console.log(età + "anni");
+
+var prezzoAlChilometro  = (0.21)
+
+var prezzoTotale = (numeroChilometri * prezzoAlChilometro)
+console.log("Prezzo Totale" + prezzoTotale + "£")
+var prezzo = (Math.random() * 10) 
+prezzo.toFixed(2)
