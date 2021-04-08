56c5cc78d4e202a85bd6e25f438e887ae4923124
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-08 17:52:43 +0200

    modifica inner html

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 var numeroChilometri = parseInt (prompt("Scrivi il numero dei chilometri che vuoi percorrere"))
 console.log(numeroChilometri + "Km");
 
-var età = parseInt (prompt("Scrivi la tua età"));
-console.log(età + "anni");
+var eta = parseInt (prompt("Scrivi la tua età"));
+console.log(eta + "anni");
 
 var prezzoAlChilometro  = (0.21);
 
 var prezzoTotale = (numeroChilometri * prezzoAlChilometro);
 console.log("Prezzo Totale" + prezzoTotale + "£");
 var prezzo = (Math.random() * 10); 
-prezzo.toFixed(2);
+prezzo.toFixed(2)
 
-if(Number.isNaN (numeroChilometri) || Number.isNaN (età)){
-     alert("Inserire cifre numeriche")
-     }
-
-if(età < 18){
-   var scontoMinorenni = ((prezzoTotale * 20) / 100);
-   console.log("Sconto Minorenni" + scontoMinorenni + "£")
+if(Number.isNaN (numeroChilometri) || Number.isNaN (eta)){
+    alert("Inserire cifre numeriche")
 }
 
-if(età > 65){
+if(eta < 18){
+    var scontoMinorenni = ((prezzoTotale * 20) / 100);
+    console.log("Sconto Minorenni" + scontoMinorenni + "£")
+    document.getElementById("sconto-18").innerHTML = "Sconto Minorenni" + 
+    ('<em>' + scontoMinorenni + '</em>') + "£"
+}
+
+if(eta > 65){
     var scontoOver = ((prezzoTotale * 40) / 100);
     console.log("Sconto Over" + scontoOver + "£")
+    document.getElementById("sconto-over").innerHTML = "Sconto Over" + ('<em>' + scontoOver + '</em>') + "£"
  }
 
- if(età > 145){
+ if(eta > 145){
     alert("Inserire età valida")
     console.log("Inserire età valida")
  }
+
+ document.getElementById("totale").innerHTML = "Prezzo del Biglietto" + 
+ ('<em>' + prezzoTotale + '</em>') + "£"
